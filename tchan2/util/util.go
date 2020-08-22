ff67015b1e1fd15ea7008fb13dfacc719ca95ebe
Frederik Gahr <10232152+fgahr@users.noreply.github.com>
2020-08-22 22:17:16 +0200

    Remove unused import, add doc comments in util.go

--- tchan2/util/util.go
+++ tchan2/util/util.go
@@ -2,11 +2,11 @@ package util
 
 import (
 	"os"
-	"path/filepath"
 
 	"github.com/pkg/errors"
 )
 
+// DirExists checks for the existence of a directory in the filesystem.
 func DirExists(dir string) (bool, error) {
 	if stat, err := os.Stat(dir); err != nil {
 		if os.IsNotExist(err) {
@@ -21,6 +21,7 @@ func DirExists(dir string) (bool, error) {
 	}
 }
 
+// FileExists checks the existence of a file in the filesystem.
 func FileExists(filePath string) (bool, error) {
 	if _, err := os.Stat(filePath); err != nil {
 		if os.IsNotExist(err) {
