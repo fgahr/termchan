7dd8eda0bae089989e722546bd83e57699a60d7e
Frederik Gahr <10232152+fgahr@users.noreply.github.com>
2020-08-25 00:04:49 +0200

    Update main to use new version.

--- main.go
+++ main.go
@@ -3,37 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/fgahr/termchan/tchan"
-	"github.com/fgahr/termchan/tchan/config"
-	"github.com/gorilla/mux"
-	"github.com/pkg/errors"
+	"github.com/fgahr/termchan/tchan2/backend"
+	"github.com/fgahr/termchan/tchan2/config"
+	"github.com/fgahr/termchan/tchan2/server"
 )
 
+func run() error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	conf := config.New(wd)
+	if err = conf.Read(); err != nil {
+		return err
+	}
+
+	db := backend.New(conf)
+	if err = db.Init(); err != nil {
+		return err
+	}
+
+	srv := server.New(conf, db)
+
+	return http.ListenAndServe(":8088", srv)
+}
+
 func main() {
-	var err error
-
-	if err = tchan.Initialize(); err != nil {
-		log.Fatal(errors.Wrap(err, "failed to initialize termchan"))
-	}
-
-	router := mux.NewRouter()
-	router.HandleFunc("/", tchan.HandleWelcome).Methods(http.MethodGet)
-	router.HandleFunc("/{board:[a-z]+}", tchan.ViewBoard).Methods(http.MethodGet)
-	router.HandleFunc("/{board:[a-z]+}/", tchan.ViewBoard).Methods(http.MethodGet)
-	router.HandleFunc("/{board:[a-z]+}", tchan.CreateThread).Methods(http.MethodPost)
-	router.HandleFunc("/{board:[a-z]+}/", tchan.CreateThread).Methods(http.MethodPost)
-	router.HandleFunc("/{board:[a-z]+}/{id:[0-9]+}", tchan.ViewThread).Methods(http.MethodGet)
-	router.HandleFunc("/{board:[a-z]+}/{id:[0-9]+}", tchan.ReplyToThread).Methods(http.MethodPost)
-
-	log.Printf("Serving HTTP on %s\n", config.Current.PortString())
-	err = http.ListenAndServe(config.Current.PortString(), router)
-	if err != nil {
-		log.Println(err)
-	}
-
-	err = tchan.Shutdown()
-	if err != nil {
+	if err := run(); err != nil {
 		log.Fatal(err)
 	}
 }
